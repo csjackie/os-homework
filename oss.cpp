80aad789f3d46be15a1005daa5acb974874c1e7f
Jackie <jmh37b@mail.umsl.edu>
2026-02-03 20:38:45 -0600

    Created oss.cpp and user.cpp

---
+++ oss.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Oss process running\n";
+	return 0;
+}
+
---
+++ user.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	std::cout << "USER PID: " << "PPID: " << "Iteration: " << "before sleeping\n";
+	std::cout << "USER PID: " << "PPID: " << "Iteration: " << "after sleeping\n";
+	return 0;
+}
+
