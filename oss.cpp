7168d81dde6d899ef29b29b0be956344c4078671
Jackie <jmh37b@mail.umsl.edu>
2026-02-06 10:44:48 -0600

    Corrected oss.cpp skeleton

--- oss.cpp
+++ oss.cpp
@@ -1,15 +1,56 @@
 #include <iostream>
-int main() {
-	//int current = 0;
-	//int total = 0;
-	int n, s, t;
-	std::cout << "Enter n, s, and t: ";
-	std::cin >> n >> s >> t;
-	std::cout << "You entered n=" << n << " s=" << s << " t=" << t;
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+
+	// sets default of n, s, and t to 1 if value not given
+        int n = 1;
+	int s = 1;
+	int t = 1;
+	int opt;
+	int current = 0;
+
+	// getopt(3) to parse options
+	while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
+		switch (opt) {
+			case 'h':
+				// outputs a help message explaining how to run then exit
+				break;
+			case 'n':
+				// convert and store the value
+				// number of total child processes
+				break;
+			case 's':
+				// convert and store the value
+				// number of allowed simultaneous child processes being run
+				break;
+			case 't':
+				// convert and store the value
+				// number of iterations completed for each child process
+				break;
+		}
+	}
+
+	// validate values are correctly entered
+	// n < 100 && > 0
+	// s < 15 && > 0
+	// t > 0
+	// s <= n
+
+	// while loop to track that the amount of simultaneous child processes is being met
 	//while (current < s) {
 		//fork();
 		//cout << "New child launched";
 		//exec(child)
 	//}
+	
+	// while loop keeps system full until launched all required children
+	// while (total < n) {
+	// 	wait();
+	// 	fork();
+	// 	total++;}
+	
+
 	return 0;
 }
