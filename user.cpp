afb4494c0b4b6eb5bfbba0f91dd02f97de37da6e
Jackie <jmh37b@mail.umsl.edu>
2026-02-07 14:28:56 -0600

    Updates to user

--- user.cpp
+++ user.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <unistd.h>
 
 int main() {
-	std::cout << "USER PID: " << "PPID: " << "Iteration: " << "before sleeping\n";
-	std::cout << "USER PID: " << "PPID: " << "Iteration: " << "after sleeping\n";
+	// takes in a single command line (t)
+	
+	// t = the number of iterations in the for loop
+	// for (i = 1; i <= t; i++) {
+		//std::cout << "USER PID: " << "PPID: " << "Iteration: " << "before sleeping\n";
+		//sleep(1)
+		//std::cout << "USER PID: " << "PPID: " << "Iteration: " << "after sleeping\n";
+	}
 	return 0;
 }
 
