6a066544067384d292b3e9806c5f751c672b6bf4
Jackie <jmh37b@mail.umsl.edu>
2026-02-08 17:18:44 -0600

    Finished user.cpp

--- user.cpp
+++ user.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <unistd.h>
+#include <cstdlib>
 
-int main() {
-	// takes in a single command line (t)
-	
-	// t = the number of iterations in the for loop
-	// for (i = 1; i <= t; i++) {
-		//std::cout << "USER PID: " << "PPID: " << "Iteration: " << "before sleeping\n";
-		//sleep(1)
-		//std::cout << "USER PID: " << "PPID: " << "Iteration: " << "after sleeping\n";
+int main(int argc, char *argv[]) {
+	int t = atoi(argv[1]);
+	for (int i = 1; i <= t; i++) {
+		std::cout << "USER PID: " << getpid() << " PPID: " << getppid() << " Iteration: " << i << " before sleeping\n";
+		sleep(1);
+		std::cout << "USER PID: " << getpid() <<  " PPID: " << getppid() << " Iteration: " << i <<  " after sleeping\n";
 	}
 	return 0;
 }
