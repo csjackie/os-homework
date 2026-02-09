89def6097d836f909bedd7fc33a00ea3e4b22616
Jackie <jmh37b@mail.umsl.edu>
2026-02-09 11:36:01 -0600

    updates

--- oss.cpp
+++ oss.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <cstdlib>
 
@@ -12,43 +11,52 @@ int main(int argc, char **argv) {
 	int t = 1;
 	int opt;
 	int current = 0;
+	int total = 0;
 
 	// getopt(3) to parse options
 	while ((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
 		switch (opt) {
 			// outputs a help message explaining how to run then exits
 			case 'h':
-				cout << "To run: ./oss -n # -s # -t #"
+				std::cout << "To run: ./oss -n # -s # -t #"
+				exit()
 				break;
 			// number of total child processes
 			case 'n':
 				// convert and store the value
-				int n = atoi(argv[2]);
+				n = atoi(optarg);
 				break;
 			// number of allowed simultaneous child processes being run
 			case 's':
 				// convert and store the value
-				int s = atoi(argv[4]);
+				s = atoi(optarg);
 				break;
 			// number of iterations completed for each child process
 			case 't':
 				// convert and store the value
-				int t = atoi(argv[6]);
+				t = atoi(optarg);
 				break;
 		}
 	}
 
 	// validate values are correctly entered
-	// n < 100 && > 0
-	// s < 15 && > 0
-	// t > 0
-	// s <= n
-
+	if (n > 100 || n <= 0) {
+		std::cer << "Invalid value for n\n";
+		exit(1);
+	}
+	if (s > 15 || s <= 0) {
+		std::cer <<
+		exit(1)}
+	if (t <= 0){
+		exit(1)}
+	if (s > n){
+		exit(1)}
+	
 	// while loop to track that the amount of simultaneous child processes is being met
 	//while (current < s) {
 		//fork();
 		//cout << "New child launched";
-		//exec(child)
+		//exec("./user", "user", t, NULL)
 	//}
 	
 	// while loop keeps system full until launched all required children
