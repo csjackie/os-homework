f6269d0175142bf55a8999c0ad7839576741443e
Jackie <jmh37b@mail.umsl.edu>
2026-02-09 20:58:07 -0600

    completed, with comments

--- user.cpp
+++ user.cpp
@@ -2,11 +2,20 @@
 #include <unistd.h>
 #include <cstdlib>
 
+/*
+ * This function displays the Child PID, Parent PID, and the number of iteration that is currently running.
+ */
+
 int main(int argc, char *argv[]) {
+	// converts string argv to int t
 	int t = atoi(argv[1]);
+	// for loop starts at 1 and continues to iterate through loop until it reaches the number t
 	for (int i = 1; i <= t; i++) {
+		// outputs the child pid, the parent pid, and the number of iteration ran before sleeping
 		std::cout << "USER PID: " << getpid() << " PPID: " << getppid() << " Iteration: " << i << " before sleeping\n";
+		// waits for 1 second
 		sleep(1);
+		// outputs the child pid, the parent pid, and the number of iteration ran after sleeping
 		std::cout << "USER PID: " << getpid() <<  " PPID: " << getppid() << " Iteration: " << i <<  " after sleeping\n";
 	}
 	return 0;
